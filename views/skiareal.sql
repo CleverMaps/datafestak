c3ba92a9c2b266f9535e34bd8162c06c664b7b74
petrsuk <petr.suk@cleveranalytics.com>
2015-11-28 09:31:52 +0100

    pohled na skiarealy

---
+++ views/skiareal.sql
@@ -0,0 +1,7 @@
+BEGIN;
+
+create view v_equipments_skiarealy as select cardmereq_key,cardmerc_desc,cardmereqh_short_name from equipments where cardmerc_desc = 'SLUZBY PRO VOLNY CAS-JINDE NESPECIFIK.' and (cardmereqh_short_name ilike '%lyž%' or cardmereqh_short_name ilike '%ski%' or cardmereqh_short_name ilike '%vlek%');
+
+comment on view v_equipments_skiarealy is 'SKI arealy - pohled na skiarealy';
+
+COMMIT;
